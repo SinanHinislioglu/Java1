8e166ddd108f4514d1d51fd30fae317d665f5285
Sinan <sinanhinis@gmail.com>
2023-04-26 09:50:59 +0200

    sinan branchi 2. kez güncellendi

--- src/kisiselKullanim/ozellik1.java
+++ src/kisiselKullanim/ozellik1.java
@@ -4,5 +4,6 @@ public class ozellik1 {
     public static void main(String[] args) {
 
         System.out.println("sinan branchinda calisiyorum");
+        System.out.println("sinan branchinde ikinci degisiklik, kendime özel calss'da");
     }
 }
